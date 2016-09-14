adf514df96dca816e1f8b02f0698261bdc71d185
webber <webber>
2016-09-14 14:03:08 +0800

    update shell

--- on_yarn_client.sh
+++ on_yarn_client.sh
@@ -5,16 +5,16 @@ PYTHON_LIB_PATH="/data/sdb1/python/pylib"
 
 
 spark-submit \
---master yarn-cluster \
---deploy-mode cluster \
+--master yarn-client \
+--deploy-mode client \
 --name sparkstreamingApp_python \
 --driver-memory 1g \
 --num-executors 10 \
 --executor-cores 6 \
 --executor-memory 3g \
 --py-files ${PYTHON_LIB_PATH}/kafka.zip,\
-${PYTHON_LIB_PATH}/fastavro.zip,${PYTHON_LIB_PATH}/elasticsearch.zip,\
-${PYTHON_LIB_PATH}/urllib3.zip,./avro.py,./es.py,./FromKafkaToEsJob.py,./FromKafkaToKafkaJob.py,./get_ssc.py \
+${PYTHON_LIB_PATH}/fastavro.zip,${PYTHON_LIB_PATH}/elasticsearch.zip,${PYTHON_LIB_PATH}/avro.zip,\
+${PYTHON_LIB_PATH}/urllib3.zip,./Serialization.py,./es.py,./FromKafkaToEsJob.py,./FromKafkaToKafkaJob.py,./get_ssc.py \
 --files ${SCHEMA_PATH}/netflow_schema.json,${SCHEMA_PATH}/netflow_enrich_schema.json,./AppConfig.json \
 --jars \
 ${LIBRARY_PATH}/zkclient-0.3.jar,${LIBRARY_PATH}/spark-streaming-kafka_2.10-1.4.0.jar,\
