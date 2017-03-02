9f39332080356dd57aeed4613aef987caae14c5f
webber <webber>
2017-03-02 16:39:44 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from FromKafkaToKafkaJob import FromKafkaToKafkaJob as Job
-import json
+from __future__ import print_function
+import json,sys
 def main():
+    if len(sys.argv)==2:
+        exec("from Jobs import %s as Job"%sys.argv[1])
+    else:
+        sys.exit("main argv Error!")
     with open("AppConfig.json") as configFile:
         config=json.loads(configFile.read())
-    job=Job(config)
+    job = Job(config)
     job.startJob()
 if __name__=="__main__":
     main()
